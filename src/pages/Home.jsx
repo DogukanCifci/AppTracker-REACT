b79e3ac171737044b423021871fd24a964c40914
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-10-26 09:39:06 +0200

    home completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,25 @@
 import GorevEkle from "../components/GorevEkle";
 import GorevleriGoster from "../components/GorevleriGoster";
-
+import { useState } from "react";
 import Data from "../helper/Data";
 
 const Home = () => {
+  const [görevler, setGörevler] = useState(Data);
+  console.log(görevler);
+  //const [görevler, setGörevler] = useState([]);
+  //setGörevler(data); //---->Böyle yaparsam sonusz döngüye girer. Girmemesi icin useEffect kullanmak zorunda kalirdim.
+
   return (
     <div>
-      <GorevEkle />
+      <header className="header">
+        <h1>TASK TRACKER</h1>
+        <button className="btn" style={{ backgroundColor: "red" }}>
+          CLOSE ADD TASKBAR
+        </button>
+      </header>
+      <GorevEkle görevler={görevler} setGörevler={setGörevler} />
 
-      <GorevleriGoster />
+      <GorevleriGoster görevler={görevler} setGörevler={setGörevler} />
     </div>
   );
 };
