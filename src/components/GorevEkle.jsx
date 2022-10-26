bb42877714f845c04d469c0c5911f47ee4d7c2eb
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-10-26 10:59:04 +0200

    tasks adding added

--- src/components/GorevEkle.jsx
+++ src/components/GorevEkle.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 
-const GorevEkle = () => {
-  return <div>GorevEkle</div>;
+const GorevEkle = ({ görevler, setGörevler }) => {
+  //console.log("asd", görevler); //6 elemenali dizimiz
+
+  const [text, setText] = useState("");
+  const [date, setDate] = useState("");
+  console.log(text);
+  console.log(date);
+
+  const yapSubmit = (event) => {
+    event.preventDefault();
+    //[] nerde olduguna dikkat et
+    setGörevler([
+      ...görevler,
+      {
+        id: görevler.length + 1,
+        text: text, //setText ile asagida güncelledigim icin burada da otomatik olarak gözükecektir
+        day: date, //setDate ile asagida güncelledigim icin burada da otomatik olarak gözükecektir
+        bittiMi: false,
+      },
+    ]);
+  };
+  return (
+    <div>
+      <form onSubmit={yapSubmit}>
+        <div className="form-control">
+          <label htmlFor="taskInput">Task</label>
+          <input
+            id="taskInput"
+            type="text"
+            placeholder="Add Task"
+            name="text"
+            value={text}
+            //her seferinde i.target.value artik text'e atanmis olur
+            onChange={(i) => setText(i.target.value)}
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="dateInput">Day & Time</label>
+          <input
+            id="dateInput"
+            type="datetime-local"
+            placeholder="Add Day"
+            name="date"
+            value={date}
+            //her seferinde i.target.value artik date'e atanmis olur.Cünkü set..in ici o
+            onChange={(e) => setDate(e.target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit" className="btn btn-submit">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default GorevEkle;
--- src/components/GorevleriGoster.jsx
+++ src/components/GorevleriGoster.jsx
@@ -1,7 +1,21 @@
 import React from "react";
-
-const GorevleriGoster = () => {
-  return <div>GorevleriGoster</div>;
+import { FaTrash } from "react-icons/fa";
+const GorevleriGoster = ({ görevler, setGörevler }) => {
+  return (
+    <div className="container">
+      {görevler.map((görev) => {
+        const { text, day, id, bittiMi } = görev;
+        return (
+          <div key={id} className={bittiMi ? "trueStil" : "falseStil"}>
+            <h3>
+              {text} <FaTrash className="thrash" />
+            </h3>
+            <h6>{day}</h6>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default GorevleriGoster;
